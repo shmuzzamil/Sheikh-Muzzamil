1c83e0b600e086a3c6de4d427f3d18e596f653fd
shmuzzamil <shmuzzamilyasin@gmail.com>
2024-08-24 01:33:40 +0500

    header added to all pages

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const mobileMenu = document.getElementById('nav-bar-wrapper')
+function loadElements() {
+    fetch('/components/header/index.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+
+loadElements()
 
 
-function openHamburgerMenu (){
+
+function openHamburgerMenu() {
+
+    const mobileMenu = document.getElementById('nav-bar-wrapper')
+
     mobileMenu.style.display = "block"
 
 }
-function closeHamburgerMenu (){
+function closeHamburgerMenu() {
+    const mobileMenu = document.getElementById('nav-bar-wrapper')
+
     mobileMenu.style.display = "none"
 
 }
+
+
